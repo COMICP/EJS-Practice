ba224f959bc9826dce070a7d1ba775925a0e50dd
COMICP <binettejaden@gmail.com>
2025-02-01 20:00:06 -0500

    Register account

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,6 +4,14 @@ const router = new express.Router();
 const envController = require("../controllers/accountController");
 const utilities = require("../utilities/");
 // Route to build inventory by classification view
+router.get(
+  "/registration",
+  utilities.handleErrors(envController.buildRegister)
+);
 router.get("/login", utilities.handleErrors(envController.buildLogin));
+router.post(
+  "/register",
+  utilities.handleErrors(envController.registerAccount)
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
-const accountRoute = require('./routes/accountRoute.js'); 
+const accountRoute = require("./routes/accountRoute.js");
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute");
 const utilities = require("./utilities/");
-
+const bodyParser = require("body-parser");
 
 /* ***********************
  * Middleware
@@ -40,6 +40,10 @@ app.use(function (req, res, next) {
   res.locals.messages = require("express-messages")(req, res);
   next();
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 /* ***********************
  * View Engine and Templates
  *************************/
